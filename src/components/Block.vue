7a410426552822ada753a82364009a40e49c15f7
Samson Moses <knomic10@gmail.com>
2022-09-13 02:25:13 +0100

    First commit

---
+++ src/components/Block.vue
@@ -0,0 +1,36 @@
+ <template >
+    <div class="block" v-if="showBlock">
+      click me  
+    </div>
+ </template>
+
+
+ <script>
+ export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('Mounted')
+        setTimeout(() => {
+            this.showBlock = true
+        }, this.delay);
+    },
+ }
+ </script>
+
+
+ <style>
+    .block{
+        width: 400px;
+        border-radius: 40px;
+        color: white;
+        background: #0faf87;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+ </style>
